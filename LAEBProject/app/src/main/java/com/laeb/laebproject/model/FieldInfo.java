bfe92cd2b4d958ae856c3ed06b8eb69e9905ecaf
Imran <imranbscs@yahoo.com>
2017-08-22 23:25:20 +0500

    done
    done

---
+++ LAEBProject/app/src/main/java/com/laeb/laebproject/model/FieldInfo.java
@@ -0,0 +1,127 @@
+package com.laeb.laebproject.model;
+
+/**
+ * Created by imrankhan on 8/22/2017.
+ */
+
+public class FieldInfo {
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getLight() {
+        return light;
+    }
+
+    public void setLight(int light) {
+        this.light = light;
+    }
+
+    public int getChange_room() {
+        return change_room;
+    }
+
+    public void setChange_room(int change_room) {
+        this.change_room = change_room;
+    }
+
+    public int getParking() {
+        return parking;
+    }
+
+    public void setParking(int parking) {
+        this.parking = parking;
+    }
+
+    public int getWifi() {
+        return wifi;
+    }
+
+    public void setWifi(int wifi) {
+        this.wifi = wifi;
+    }
+
+    public String getNearby() {
+        return nearby;
+    }
+
+    public void setNearby(String nearby) {
+        this.nearby = nearby;
+    }
+
+    public int getWater() {
+        return water;
+    }
+
+    public void setWater(int water) {
+        this.water = water;
+    }
+
+    public String city;
+    public String latitude;
+    public String longitude;
+    public String size;
+    public String capacity;
+    public String type;
+    public int light;
+    public int change_room;
+    public int parking;
+    public int wifi;
+    public String nearby;
+    public int water;
+
+}
