a778419924d73dd3912b56138e11b0fc7fb5d759
DESKTOP-6V9E301\tariq <tariqr93@gmail.com>
2017-08-27 02:32:32 +0500

    screens

--- LAEBProject/app/src/main/java/com/laeb/laebproject/team_fragments/FragmentCreateTeam.java
+++ LAEBProject/app/src/main/java/com/laeb/laebproject/team_fragments/FragmentCreateTeam.java
@@ -1,15 +1,26 @@
 package com.laeb.laebproject.team_fragments;
 
+import android.app.Dialog;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.laeb.laebproject.BookingActivity;
 import com.laeb.laebproject.R;
 import com.laeb.laebproject.fragments_booking.FragmentSearchFacilities;
+
+import java.util.ArrayList;
 
 /**
  * Created by tariq on 8/26/2017.
@@ -21,14 +32,33 @@ public class FragmentCreateTeam extends Fragment {
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_createteam, container, false);
-//        TextView searchTv = (TextView) v.findViewById(R.id.search_text);
-//        searchTv.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                FragmentSearchFacilities fragment = new FragmentSearchFacilities();
-//                ((BookingActivity) getActivity()).addFragment(fragment);
-//            }
-//        });
+        LinearLayout next = (LinearLayout) v.findViewById(R.id.nextBtn);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addDialog();
+            }
+        });
         return v;
     }
+
+    public void addDialog(){
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.color_dialog);
+        dialog.show();
+        TextView okBtn = (TextView) dialog.findViewById(R.id.done);
+
+//        final String finalRateStr = rateStr;
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                    dialog.dismiss();
+
+            }
+        });
+
+    }
+
 }
